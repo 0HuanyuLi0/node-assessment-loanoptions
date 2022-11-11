c6a56a5e559141707954e0c18138323f67cb68b8
Huanyu Li <lihuanyu1024@outlook.com>
2022-11-12 00:44:19 +1030

    add readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Task 1 - API - React
+### Description
+Create a ﬁle which when executed from the command line, prints the values based on 
+the arguments passed.
+API Endpoint: https://api.publicapis.org/entries
+
+### How to run
+- `node main.js category limit`
+    e.g. run `node main.js Weather 5`
+
+### Features
+- `category` parameter is case insensitives
+- Results are sorted alphabetically by value of "API" in descending order
+
+
+### Instructions
+- Write code inside the ﬁle which fetches data from the API and prints the values 
+of ‘API’ decreasing alphabetically
+- The ﬁle accepts two arguments ‘category’ and ‘limit’
+- Print the results matching the ‘category’ and ‘limit’ is the number of results to 
+print eg: php ﬁle.php Weather 5
+- If no records matches the category, print ‘No results
--- main.js
+++ main.js
@@ -2,17 +2,37 @@
 const axios = require('axios')
 const url = `https://api.publicapis.org/entries`
 
-const getData = async() => {
-    try{
+const getData = async () => {
+    try {
         const res = await axios.get(url)
-        res.data.entries.sort((a,b)=>b.API.localeCompare(a.API,{sensitivity: 'base'}))
-        console.log(res.data);
+        res.data.entries.sort((a, b) => b.API.localeCompare(a.API, { sensitivity: 'base' }))
         return res.data
-    }catch(err){
-        console.log('Error at get data',err);
+    } catch (err) {
+        console.log('Error at get data', err);
     }
 }
 
 
+const showResults = async () => {
+    let [category, limit, ...others] = process.argv.slice(2)
+    const { count, entries } = await getData()
+    let results = []
+    entries.every(item => {
+        if (item.Category.toUpperCase() === category.toUpperCase()) {
+            limit -= 1
+            results.push(item)
+        }
+        // false -> stop loop 
+        if (limit < 1) return false
+        else return true
+    })
+    if (results.length < 1) {
+        console.log(`No results`)
+        return `No results`
+    } else {
+        console.log(results)
+        return results
+    }
+}
 
-console.log(process.argv.slice(2));
+showResults()
